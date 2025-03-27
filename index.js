2732c6c9de98364ad2f2a9feaf618d0d327d51bf
tshims <bentshims21@gmail.com>
2025-03-27 13:56:19 +0200

    debut du responsive design

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 document.addEventListener(`DOMContentLoaded`,()=>{
 
-    const logoContainer = document.querySelector(`.logos-container`)
-    const logos = document.querySelector(`.partner-logos`);
-    let logoClones = logos.cloneNode(true);
-    logoContainer.appendChild(logoClones);
-    logoContainer.classList.add(`flex`,`border`,`border-white`);
+    // burger menu
 
+    const overlay = document.querySelector(`.overlay`)
+    const menu_Burger = document.querySelectorAll(`.burger`);
+    menu_Burger.forEach(burger => {
+        burger.addEventListener(`click`,()=>{
+            // overlay.classList.add(`translate`);
+            overlay.classList.remove(`none`);
+        })
+    });
+    
+    
+
+    // animation logos continue 
+
+    const container = document.querySelector(`.logos-container`);
+    const logos = document.querySelector(`.partner-logos`);
+    const logo_Clones = logos.cloneNode(true);
+    container.appendChild(logo_Clones);
+    console.log(container,logos,logo_Clones);
+    
+
+
+
+
+    
 
 
 
